0538fb46999e755f7e5c6365f17d9c4feb972331
Kreso <kreso@ubuntu.(none)>
2013-01-11 13:09:42 +0000

    Rack ing

--- app_sinatra.rb
+++ app_sinatra.rb
@@ -7,12 +7,12 @@ require "mustache/sinatra"
 class App < Sinatra::Base
 	register Mustache::Sinatra
 	require "./views/layout"
-#=begin
+=begin
 	set :mustache, {
 		:views => './views/',
 		:templates => './views/'
 	}
-#=end
+=end
 	@@rows = [
 		{"name"=>"Chris", "idea"=>"Space"},
 		{"name"=>"Bill", "idea"=>"Time"},
@@ -31,3 +31,11 @@ class App < Sinatra::Base
 		mustache :index, :locals => {:rows => @@rows}
 	end
 end
+
+=begin *** when running as "ruby app_sinatra/rb". On Exit, it doesn't release the process; sudo lsof -i 
+use Rack::ShowExceptions
+Rack::Handler::WEBrick.run(
+  App.new,
+  :Port => 9192
+)
+=end
